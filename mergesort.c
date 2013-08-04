b4bcfa268bbaf423fc0c74612cfa1d8248600cd3
Goobyalus <jsaina@umd.edu>
2013-08-03 23:34:10 -0400

    created mergesort.c with mergesort_int skeleton

---
+++ mergesort.c
@@ -0,0 +1,60 @@
+/*******************************************************************************
+An implementation of mergesort in C
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *Header declarations
+ */
+
+int mergesort_int(int **arr, int len);
+
+
+
+
+
+
+
+int main(int argc, char **argv){
+	
+	return 0;
+}
+
+
+/*
+ *Sort the first 'len' elements of integer array pointed to by 'arr' in place.
+ *
+ *'arr' is a pointer to the integer array to sort.
+ *'len' is the number of elements to sort, and must be >= 1.
+ */
+int mergesort_int(int **arr, int len){
+	
+	//TODO: add required variables here
+	int temp = 0;
+
+
+	//Check for valid length	
+	if(len < 0)
+		return NULL;
+
+
+	//Base cases
+	switch(len){
+	case 0:
+	case 1:	return 1;
+
+	case 2:	temp = (*arr)[0];
+		if( (*arr)[1] < temp){
+			(*arr)[0] = (*arr)[1];
+			(*arr)[1] = temp;
+		}
+		return 1;
+	}
+
+	//TODO: implement recursive calls
+
+	//TODO: implement merge
+	
+}
